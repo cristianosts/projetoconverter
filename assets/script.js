f199191bf7b853c620d0e04f030ea9fd2f37194e
cristianosts <cristianosantosreal@gmail.com>
2024-12-15 18:15:18 -0300

    act

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+let dolar = 5.1
+
+let usdInput = document.querySelector('#usd')
+let brlInput = document.querySelector('#brl')
+
+// capturar evento no input
+usdInput.addEventListener('keyup', () => {
+    
+})
+
+brlInput.addEventListener('keyup', () => {
+
+})
+
+//função de converter 
+
+
